13ae983a454b77fe80c06e973100ab929135d3e5
vincentcccheng <vincent-cheng@live.com>
2020-07-10 15:02:54 +0000

    static

--- flask_qa/__init__.py
+++ flask_qa/__init__.py
@@ -1,33 +1,40 @@
-from flask import Flask 
-from flask_sqlalchemy import SQLAlchemy 
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-from flask_mysqldb import MySQL 
+from flask_mysqldb import MySQL
 
 db = SQLAlchemy()
 mysql = MySQL()
 
 def create_app():
-    
+
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'thisismysecretkeydonotstealit'
     app.config['MAX_CONTENT_LENGTH'] = 90 * 1024 * 1024
+
+    # for windows
+
     #app.config['UPLOAD_FOLDER'] = '.\/flask_qa\/static'
-    app.config['UPLOAD_FOLDER'] = '/static'
-    
+    #app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:mmgoverseas@localhost/flask'
 
-    #app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://mflask:mmgoverseas@mflask.mysql.pythonanywhere-services.com/mflask$default'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:mmgoverseas@localhost/flask'
+    #app.config['MYSQL_HOST'] = 'localhost'
+    #app.config['MYSQL_USER'] = 'root'
+    #app.config['MYSQL_PASSWORD'] = 'mmgoverseas'
+    #app.config['MYSQL_DB'] = 'flask'
+    #mysql = MySQL(app)
 
-    app.config['MYSQL_HOST'] = 'localhost'
-    app.config['MYSQL_USER'] = 'root'
+    ## for Pythonanywhere ##
+
+    app.config['UPLOAD_FOLDER'] = '/home/mflask/flask_qa_app/flask_qa/static'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://mflask:mmgoverseas@mflask.mysql.pythonanywhere-services.com/mflask$default'
+    app.config['MYSQL_HOST'] = 'mflask.mysql.pythonanywhere-services.com'
+    app.config['MYSQL_USER'] = 'mflask'
     app.config['MYSQL_PASSWORD'] = 'mmgoverseas'
-    app.config['MYSQL_DB'] = 'flask'
+    app.config['MYSQL_DB'] = 'mflask$default'
+
     mysql = MySQL(app)
-
-    #print('app.config is' + app.config['SQLALCHEMY_DATABASE_URI']) 
-
     db.init_app(app)
-
+   
     login_manager = LoginManager()
     login_manager.login_view = 'auth.login'
     login_manager.init_app(app)
