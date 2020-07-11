99e6b826844435ff67a5fccc6533f45f19d467de
vincentcccheng <vincent-cheng@live.com>
2020-07-11 15:51:28 +0000

    Include static

--- flask_qa.bak/auth.py
+++ flask_qa.bak/auth.py
@@ -2,13 +2,24 @@ from flask import Blueprint, render_template, redirect, url_for, request, flash
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import login_user, logout_user, login_required
 from .models import User
-from . import db
+from . import db, mysql
+from werkzeug.utils import secure_filename  # fileupload
+import os  # fileupload
+from flask import current_app
 
 auth = Blueprint('auth', __name__)
+
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif', 'xlsx'])
+
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
 
 @auth.route('/login')
 def login():
     return render_template('login.html')
+
 
 @auth.route('/login', methods=['POST'])
 def login_post():
@@ -18,7 +29,7 @@ def login_post():
 
     user = User.query.filter_by(email=email).first()
 
-    if not user or  not check_password_hash(user.password, password):
+    if not user or not check_password_hash(user.password, password):
         flash('Please check your login details and try again.')
         return redirect(url_for('auth.login'))
 
@@ -26,9 +37,11 @@ def login_post():
 
     return redirect(url_for('main.profile'))
 
+
 @auth.route('/signup')
 def signup():
     return render_template('signup.html')
+
 
 @auth.route('/signup', methods=['POST'])
 def signup_post():
@@ -42,12 +55,52 @@ def signup_post():
         flash('Email address already exists.')
         return redirect(url_for('auth.signup'))
 
-    new_user = User(email=email, name=name, password=generate_password_hash(password, method='sha256'))
+    new_user = User(email=email, name=name,
+                    password=generate_password_hash(password, method='sha256'))
 
     db.session.add(new_user)
     db.session.commit()
 
     return redirect(url_for('auth.login'))
+
+
+@auth.route('/listuser')
+def listuser():
+
+    cur = mysql.connection.cursor()
+
+    #cur.execute('''CREATE TABLE example (id INTEGER, name VARCHAR(20))''')
+    #cur.execute('''INSERT INTO example VALUES (1, 'Anthony')''')
+    #cur.execute('''INSERT INTO example VALUES (2, 'Billy')''')
+    # mysql.connection.commit()
+
+    cur.execute('''SELECT * FROM user''')
+    results = cur.fetchall()
+    # print(results)
+    return (results[0][1])
+
+
+@auth.route('/uploadform', methods=['GET', 'POST'])
+def uploadform():
+    return render_template('upload.html')
+
+
+@auth.route('/upload', methods=['POST'])
+def upload():
+    upload_folder = current_app.config['UPLOAD_FOLDER']
+    if request.method == 'POST':
+        # check if the post request has the files part
+        if 'files[]' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        files = request.files.getlist('files[]')
+        for file in files:
+            if file and allowed_file(file.filename):
+                filename = secure_filename(file.filename)
+                file.save(os.path.join(upload_folder, filename))
+        flash('File(s) successfully uploaded')
+        return redirect('/')
+
 
 @auth.route('/logout')
 @login_required
